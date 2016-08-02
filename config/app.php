278634886469ef5a671a6a3a7fd55c7898ce57a1
leggetter <phil@leggetter.co.uk>
2016-08-02 16:14:47 +0100

    Adding Nexmo Laravel Package

--- config/app.php
+++ config/app.php
@@ -155,6 +155,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        Nexmo\Laravel\NexmoServiceProvider::class
 
     ],
 
---
+++ config/nexmo.php
@@ -0,0 +1,29 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Credentials
+    |--------------------------------------------------------------------------
+    |
+    | If you're using API credentials, change these settings. Get your
+    | credentials from https://dashboard.nexmo.com | 'Settings'.
+    |
+    */
+
+    'api_key' => env('NEXMO_API_KEY'),
+    'api_secret' => env('NEXMO_API_SECRET'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Signature Secret
+    |--------------------------------------------------------------------------
+    |
+    | If you're using a signature secret, use this section.
+    |
+    */
+
+    //'api_key' => 'API_KEY',
+    //'signature_secret' => 'SECRET'
+
+];
